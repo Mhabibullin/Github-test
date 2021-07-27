e998eb75b573ac9307c3179d0fba67f95d2662ea
zimmermanul <habib@e1.ru>
2021-07-27 17:18:42 +0500

    Completed first test

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -1,4 +1,20 @@
 package guru.qa;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
 public class FirstTest {
+
+    @Test
+    void shouldFindSelenideRepository(){
+        Selenide.open("https://github.com");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $$(".repo-list li").first().$("a").click();
+        $("h1").shouldHave(Condition.text("selenide / selenide"));
+
+    }
 }
